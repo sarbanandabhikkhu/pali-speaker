4c8ac6e04244183f1b2ffc575131db14a88f2757
SarbaNanda Bhikkhu <105801170+sarbanandabhikkhu@users.noreply.github.com>
2025-01-04 09:58:11 +0600

    Create fetchData.js

---
+++ utils/fetchData.js
@@ -0,0 +1,16 @@
+async function fetchData(path, cb) {
+  try {
+    const res = await fetch(path);
+    const txt = await res.text();
+    const splited = txt.split(/\r\n\r\n/g);
+    splited.forEach((item) => {
+      const section = item.trim().split(/\r\n/g);
+      // regexp[section[0]] = new RegExp(section[1].replace(/\//g, ""));
+      cb(section);
+    });
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+export default fetchData;
