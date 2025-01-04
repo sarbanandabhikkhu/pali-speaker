93c95d36c3a5465dea808449ef5e77b3e3ae4b51
SarbaNanda Bhikkhu <105801170+sarbanandabhikkhu@users.noreply.github.com>
2025-01-04 10:52:42 +0600

    Update fetchData.js

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -2,11 +2,11 @@ async function fetchData(path, cb) {
   try {
     const res = await fetch(path);
     const txt = await res.text();
-    const splited = txt.split(/\r\n\r\n/g);
+    const splited = txt.split(/\r\n\r\n/);
     splited.forEach((item) => {
-      const section = item.trim().split(/\r\n/g);
+      const section = item.trim().split(/\r\n/);
       // regexp[section[0]] = new RegExp(section[1].replace(/\//g, ""));
-      cb(section.trim());
+      cb(section);
     });
   } catch (err) {
     console.error("Error:", err);
