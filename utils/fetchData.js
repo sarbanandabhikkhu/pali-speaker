3bac30331c7e8f94b6b6509e6e9554e438567c7a
SarbaNanda Bhikkhu <105801170+sarbanandabhikkhu@users.noreply.github.com>
2025-01-04 10:50:30 +0600

    Update fetchData.js

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -6,7 +6,7 @@ async function fetchData(path, cb) {
     splited.forEach((item) => {
       const section = item.trim().split(/\r\n/g);
       // regexp[section[0]] = new RegExp(section[1].replace(/\//g, ""));
-      cb(section);
+      cb(section.trim());
     });
   } catch (err) {
     console.error("Error:", err);
